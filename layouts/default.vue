842ff5f16ee1e7e6173e7c974028234adc0de6d2
Holger Mueller <9jholger@gmail.com>
2022-07-28 17:22:10 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <v-app-bar app>
+          <h1>Holger Mueller</h1>
+        </v-app-bar>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span
+        >&copy; {{ new Date().getFullYear() }} Holger Mueller &middot; Built
+        with Nuxtjs</span
+      >
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data: () => ({}),
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center"> </v-card>
+      <v-card>
+        <v-card-title class="headline"> Title goes here </v-card-title>
+        <v-card-text>
+          <hr class="my-3" />
+          Content goes here
+        </v-card-text>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
