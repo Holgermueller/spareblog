7661f17a7bb5f33e65a39e8294d8b1cd9feafc7e
Holger Mueller <9jholger@gmail.com>
2022-07-28 22:01:25 -0500

    got markdowm on dom as html

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,61 @@
 <template>
   <div id="home">
-    <p>
-      {{ blogs[0] }}
-    </p>
-    <v-card>
-      <v-card-title class="headline">
-        <p>
-          {{ blogs[0].title }}
-        </p>
-        <v-spacer></v-spacer>
-        <p>
-          {{ formatDate(blogs[0].date) }}
-        </p>
-      </v-card-title>
-      <v-card-text>
-        <hr class="my-3" />
+    <section id="newestPost">
+      <v-card class="newest-blog" elevation="0">
+        <v-card-title class="headline">
+          <p>
+            {{ blogs[0].title }}
+          </p>
+          <v-spacer></v-spacer>
+          <p>
+            {{ formatDate(blogs[0].date) }}
+          </p>
+        </v-card-title>
+        <v-card-text>
+          <hr class="my-3" />
 
-        {{ blogs[0].body }}
-      </v-card-text>
-      <v-card-actions> </v-card-actions>
-    </v-card>
+          <article>
+            <nuxt-content :document="blogs[0]"></nuxt-content>
+          </article>
+        </v-card-text>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+    </section>
+
+    <section id="otherRecentPosts" class="other-recent-posts">
+      <h2>other recent posts appear here</h2>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-hover v-slot:default="{ hover }">
+              <v-card :elevation="hover ? 12 : 0" class="recent-post">
+                <v-card-title> title </v-card-title>
+                <hr />
+                <v-card-text> text </v-card-text>
+              </v-card>
+            </v-hover>
+          </v-col>
+          <v-col>
+            <v-hover v-slot:default="{ hover }">
+              <v-card :elevation="hover ? 12 : 0" class="recent-post">
+                <v-card-title> title </v-card-title>
+                <hr />
+                <v-card-text> text </v-card-text>
+              </v-card>
+            </v-hover>
+          </v-col>
+          <v-col>
+            <v-hover v-slot:default="{ hover }">
+              <v-card :elevation="hover ? 12 : 0" class="recent-post">
+                <v-card-title> title </v-card-title>
+                <hr />
+                <v-card-text> text </v-card-text>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
   </div>
 </template>
 
@@ -44,4 +80,14 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.newest-blog {
+  width: 95%;
+  margin: auto;
+}
+
+.other-recent-posts {
+  width: 95%;
+  margin: auto;
+}
+</style>
